b5a0296e1f4ca7ce6d366f0d4576fdf7fcadaca4
KhDang1001 <52146179+KhDang1001@users.noreply.github.com>
2019-09-14 21:52:22 -0700

    Lab1

--- baiTap1.java
+++ baiTap1.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Lap2;
+package Lap1;
 
 import java.util.Scanner;
 
@@ -13,25 +13,18 @@ import java.util.Scanner;
  */
 public class baiTap1 {
     public static void main(String[] args){
-        int a, b ;
-        double nghiem;
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); 
         
-        System.out.println("Nhập vào số a: ");
-        a = scanner.nextInt();
-        System.out.println("Nhập vào số b: ");
-        b = scanner.nextInt();
+        System.out.print("Họ và tên: ");
+        String hoTen = scanner.nextLine(); 
         
-        if(a == 0){
-            if(b == 0){
-                System.out.println("Phương trình này có vô số nghiệm.");
-            }else{
-                System.out.println("Phương trình vô nghiệm.");
-            }
-        }else{
-            nghiem = -b/a;
-            System.out.println("Phương trình có nghiệm x = " +nghiem);
-        }
+        System.out.print("Điểm TB: ");
+        double diemTB = scanner.nextDouble();
+        
+        System.out.println("=============");
+        
+        System.out.printf("%s: %.1f diem", hoTen, diemTB);
+        
     }
     
 }
--- baiTap2.java
+++ baiTap2.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Lap2;
+package Lap1;
 
 import java.util.Scanner;
 
@@ -13,36 +13,24 @@ import java.util.Scanner;
  */
 public class baiTap2 {
     public static void main(String[] args){
-        System.out.println("Vui long nhap vao he so a, b, c:");
-        Scanner scanner = new Scanner(System.in);
-        
-        double a = scanner.nextDouble();
-        double b = scanner.nextDouble();
-        double c = scanner.nextDouble();
-        
-        if (a == 0){
-            System.out.println("phuong trinh co nghiem la:" +(-c/b));
-        }else{
-            double delta = b*b - 4*a*c;
-            
-            if(delta < 0){
-                System.out.println("phuong trinh vo nghiem");
-            }else if (delta == 0){
-            double x = -b/2*a;
-            System.out.println("Phuong trih co nghiem kep:" +x);
-        }else{
-                double x1 =(-b -  Math.sqrt(delta))/2*a;
-                double x2 =(-b +  Math.sqrt(delta))/2*a;
-                
-                System.out.println("Phuong trinh co 2 nghiem phan biet la;");
-                System.out.println("x1 = " +x1);
-                System.out.println("x2 = " +x2);
-                }
-        }
+        int canh;
         
         
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Chieu dai hinh chu nhat: ");
+        float fChieuDai = scanner.nextFloat();
         
+        System.out.print("Chieu rong hinh chu nhat: ");
+        float fChieuRong = scanner.nextFloat();
+        
+        float ChuVi, DienTich, CanhNhoNhat;
+        ChuVi = (fChieuDai + fChieuRong)*2;
+        DienTich = fChieuDai * fChieuRong;
+        CanhNhoNhat = Math.min(fChieuDai, fChieuRong);
+        
+        System.out.println("Chu Vi Hinh chu Nhat la: " +ChuVi);
+        System.out.println("Dien Tich Hinh chu Nhat la : " +DienTich);
+        System.out.println("Canh Nho Nhat la: " +CanhNhoNhat);
     }
-    
     
 }
